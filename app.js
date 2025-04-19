39ae7dd123b2121d780652e5b2144996d1ab4d6e
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-04-19 21:21:00 +0300

    added routing responses

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const app = express();
 
 app.get("/favicon.ico", (req, res) => res.status(204));
 
-app.use((req, res, next) => {
-  console.log("In first middleware");
-  next();
+app.use('/add-product', (req, res, next) => {
+  console.log("Add-product middleware");
+  res.send("<h1>The add product page</h1>");
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log("In second middleware");
   res.send("<h1>Hello from Express.js</h1>");
 });
