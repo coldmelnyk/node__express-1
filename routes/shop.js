4bfc765658450bfdd020747643ec1bea54cff91e
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-04-28 00:59:28 +0300

    cleaned project up and stayed on ejs as a temp engine

--- routes/shop.js
+++ routes/shop.js
@@ -8,15 +8,11 @@ router.get("/favicon.ico", (req, res) => res.status(204));
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  const doesProductsExists = products.length > 0;
 
   res.render("shop", {
-    doesProductsExists,
     products,
     pageTitle: "Shop",
     path: "/",
-    activeShop: true,
-    productCSS: true,
   });
 });
 
