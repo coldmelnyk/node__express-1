b8342a9cd5506fa26248f29f42034afa58170fa0
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-04-28 00:22:03 +0300

    changed temp engine to hbs

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,16 @@ router.get("/favicon.ico", (req, res) => res.status(204));
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
+  const doesProductsExists = products.length > 0;
 
-  res.render("shop", { products, pageTitle: "Shop", path: "/" });
+  res.render("shop", {
+    doesProductsExists,
+    products,
+    pageTitle: "Shop",
+    path: "/",
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 exports.routes = router;
