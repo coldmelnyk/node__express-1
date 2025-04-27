d440b0fddcad761e74285f80935a6facd4532f48
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-04-27 20:45:48 +0300

    added dynamic html with pug template engine

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ const path = require("path");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopData = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+app.use("/admin", adminData.routes);
+app.use(shopData.routes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "not-found.html"));
+  res.status(404).render("not-found", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3030);
